fc1c8efbde2da11fc4c34e92a77e9930bc2b2f4c
EKKOING <nicholas.lorentzen@gmail.com>
2021-04-14 23:13:47 -0400

    add views

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about-me.html'
+
+class PortfolioPageView(TemplateView):
+    template_name = 'projects.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+class SocialsPageView(TemplateView):
+    template_name = 'socials.html'
