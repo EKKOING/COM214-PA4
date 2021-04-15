af4c70bf6e47e6a230e49eba0b658daa9d6a6987
EKKOING <nicholas.lorentzen@gmail.com>
2021-04-15 00:02:05 -0400

    add tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_portfolio_page_status_code(self):
+        response = self.client.get('/portfolio/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_status_code(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_socials_page_status_code(self):
+        response = self.client.get('/socials/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_bootstrapless_page_status_code(self):
+        response = self.client.get('/socials/bootstrapless/')
+        self.assertEqual(response.status_code, 200)
